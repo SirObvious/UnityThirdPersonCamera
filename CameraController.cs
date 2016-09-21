932319eda786cfb8ea1a45e4a972ebc2cba24ce2
SirObvious <Pascal.Worreschk@gmail.com>
2016-09-21 22:14:56 +0200

    Horizontal | Vertical Input & Rotation First Implementation

--- CameraController.cs
+++ CameraController.cs
@@ -3,13 +3,50 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
-	// Use this for initialization
+    //Public References
+    public Transform playerTransform;
+
+    //Public Parameter
+    public float rotationSpeed;
+
+
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
 	
+	void Update () {
+        Vector2 axisInput = GetAnalogInput();
+        RotationHorizontal(axisInput.x);
+        RotationVertical(axisInput.y);
+        LookAtPlayer();
+
 	}
+
+
+    private Vector2 GetAnalogInput()
+    {
+        float xInput = Input.GetAxisRaw("JoystickRightX");
+        float yInput = Input.GetAxisRaw("JoystickRightY");
+        return new Vector2(xInput, yInput);
+    }
+
+    private void RotationHorizontal(float horizontalInput)
+    {
+        this.transform.RotateAround(playerTransform.position, Vector3.up, rotationSpeed * horizontalInput * Time.deltaTime);
+    }
+
+    private void RotationVertical(float verticalInput)
+    {
+        this.transform.RotateAround(playerTransform.position, Vector3.right, rotationSpeed * verticalInput * Time.deltaTime);
+    }
+
+    private void LookAtPlayer()
+    {
+        this.transform.LookAt(playerTransform.position);
+    }
+
+
+
+
 }
